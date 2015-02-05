11131eee53cd7bf5b8ef5a35357d00fb8ef658f1
utkarsh578 <utkarsh578@gmail.com>
2015-02-06 02:53:51 +0530

    hello

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -25,7 +25,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'obscura',
 			'username'  => 'root',
-			'password'  => 'obscura@utkarsh',
+			'password'  => 'evm',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
